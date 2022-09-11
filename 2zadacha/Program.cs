e3989a4b2cf0b23fef5f84f95ae4fcfea8343fc9
Vika <vica_che@mail.ru>
2022-09-12 02:44:17 +0300

    2zadacha

---
+++ 2zadacha/Program.cs
@@ -0,0 +1,23 @@
+﻿int summ = 0;
+Random Rnd = new Random();
+int[] a = new int[10];
+Console.Write("Исходный массив:");
+
+
+for (int i = 0; i < a.Length; i++)
+{
+a[i] = Rnd.Next(1, 10);
+Console.Write(a[i] + " ");
+}
+Console.WriteLine();
+Console.Write("Результат: ");
+
+for (int i = 0; i < a.Length; i++)
+{
+if (a[i] % 2 == 1)
+{
+summ = a[i] + summ ;
+}
+
+}
+Console.Write(summ);
